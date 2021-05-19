6f16516ca25fdd7f1c12f2c84cd31a6b4d6e0e53
Rodrigo Peixoto <rodrigogat5@hotmail.com>
2021-05-19 00:03:02 -0300

    entrega 2 crud colaboradores

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,37 +1,39 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Foundation where
 
-import Import.NoFoundation
 import Database.Persist.Sql (ConnectionPool, runSqlPool)
-import Yesod.Core.Types     (Logger)
+import Import.NoFoundation
+import Yesod.Core.Types (Logger)
 
 data App = App
-    { appSettings    :: AppSettings
-    , appStatic      :: Static 
-    , appConnPool    :: ConnectionPool 
-    , appHttpManager :: Manager
-    , appLogger      :: Logger
-    }
+  { appSettings :: AppSettings,
+    appStatic :: Static,
+    appConnPool :: ConnectionPool,
+    appHttpManager :: Manager,
+    appLogger :: Logger
+  }
 
 mkYesodData "App" $(parseRoutesFile "config/routes")
 
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
 instance Yesod App where
-    makeLogger = return . appLogger
+  makeLogger = return . appLogger
 
 instance YesodPersist App where
-    type YesodPersistBackend App = SqlBackend
-    runDB action = do
-        master <- getYesod
-        runSqlPool action $ appConnPool master
+  type YesodPersistBackend App = SqlBackend
+  runDB action = do
+    master <- getYesod
+    runSqlPool action $ appConnPool master
 
 instance RenderMessage App FormMessage where
-    renderMessage _ _ = defaultFormMessage
+  renderMessage _ _ = defaultFormMessage
 
 instance HasHttpManager App where
-    getHttpManager = appHttpManager
+  getHttpManager = appHttpManager
---
+++ src/Handler/Colaborador.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Handler.Colaborador where
+
+import Import
+
+formColaborador :: Form Colaborador
+formColaborador =
+  renderDivs $
+    Colaborador
+      <$> areq textField "Nome: " Nothing
+      <*> areq textField "Email: " Nothing
+      <*> areq textField "Telefone: " Nothing
+
+getColaboradorR :: Handler Html
+getColaboradorR = do
+  (widget, _) <- generateFormPost formColaborador
+  msg <- getMessage
+  defaultLayout $
+    [whamlet|
+      $maybe mensa <- msg
+        <div>
+          ^{mensa}
+      <h1>
+        Cadastro de Colaborador
+
+      <form method=post action=@{ColaboradorR} >
+        ^{widget}
+        <input type="submit" value="Cadastrar">      
+    |]
+
+postColaboradorR :: Handler Html
+postColaboradorR = do
+  ((result, _), _) <- runFormPost formColaborador
+  case result of
+    FormSuccess colaborador -> do
+      runDB $ insert colaborador
+      setMessage
+        [shamlet|
+
+        <div>
+          Cliente incluido com sucesso !
+      |]
+      redirect ColaboradorR
+    _ -> redirect HomeR
+
+getPerfilR :: ColaboradorId -> Handler Html
+getPerfilR cid = do
+  colaborador <- runDB $ get404 cid
+  defaultLayout $ do
+    addStylesheet (StaticR css_bootstrap_css)
+    addStylesheet (StaticR css_estilo_css)
+    $(whamletFile "templates/Layout/header.hamlet")
+
+    $(whamletFile "templates/Colaborador/perfil.hamlet")
+
+getListaColabR :: Handler Html
+getListaColabR = do
+  colaboradores <- runDB $ selectList [] [Asc ColaboradorNome]
+  defaultLayout $ do
+    addStylesheet (StaticR css_bootstrap_css)
+    addStylesheet (StaticR css_estilo_css)
+    $(whamletFile "templates/Layout/header.hamlet")
+    $(whamletFile "templates/Colaborador/colaboradores.hamlet")
+    $(whamletFile "templates/Layout/footer.hamlet")
+
+postApagarColabR :: ColaboradorId -> Handler Html
+postApagarColabR cid = do
+  runDB $ delete cid
+  redirect ListaColabR
